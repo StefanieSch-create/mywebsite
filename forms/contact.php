fc98d2de8ab3b325cd29028020d30ffbce0464c0
StefanieSch-create <76407335+StefanieSch-create@users.noreply.github.com>
2021-01-05 22:56:50 +0100

    Update contact.php
    comment last lines

--- forms/contact.php
+++ forms/contact.php
@@ -32,7 +32,7 @@
     'port' => '465'
   );
 
-
+/*
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
